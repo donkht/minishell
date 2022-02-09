0b4a80812fb6a6b0125de22cee4e92faf8f80fa2
Neelix Leyton <nleyton@student.21-school.ru>
2022-02-09 19:13:13 +0300

    can execute apps without arguments

--- minishell.c
+++ minishell.c
@@ -2,15 +2,23 @@
 
 int	main(int argc, char **argv, char **envp)
 {
-	char *str;
-
+	char	*str;
+	pid_t	p;
 	(void)argc;
 	(void)argv;
 	(void)envp;
 	while(1)
 	{
-		str = readline("minishell>");
-		printf("%s\n", str);
+		str = readline("minishell> ");
+		add_history(str);
+		// printf("\n");
+		p = fork();
+		if (p == 0)
+		{
+			str = ft_strjoin("/bin/", str);
+			execve(str, argv, envp);
+		}
+		wait(0);
 		free(str);
 	}
 
